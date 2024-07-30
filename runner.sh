39af325dc5acfd90525c473fa32d7809b60986be
Shandon Anderson <shandon@shandon.codes>
2024-07-30 00:27:22 +0000

    Added hjelper running script

---
+++ runner.sh
@@ -0,0 +1,12 @@
+# Given TC
+./app BOOK A0 1
+./app CANCEL A0 1
+./app BOOK A0 1
+./app BOOK A0 1
+./app BOOK A1 1
+./app BOOK A2 4
+./app BOOK A5 1
+./app BOOK A6 3
+./app BOOK A6 4
+./app BOOK A8 1
+./app BOOK U1 1
